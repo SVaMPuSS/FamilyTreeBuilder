4c84689909e2dae2253b83e1bf39ca37281c145e
Anton <Totonto85@gmail.com>
2022-05-11 00:13:45 +0300

    Add project files.

---
+++ Model/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace FamilyTreeBuilder.Model
+{
+    public class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? DeathDate { get; set; }
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        private Point position;
+
+        public Point Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+            }
+        }
+
+        public bool CheckClick(Point location) => !(location.X < Position.X && location.Y > Position.Y && Position.X+Width < location.X && Position.Y + Height < location.Y);
+
+        public List<Human> Children { get; set; }
+        public List<Human> Parents { get; set; }
+        public Human()
+        {
+            Children = new List<Human>();
+            Parents = new List<Human>();
+        }
+
+    }
+}
---
+++ UserContols/Field.xaml.cs
@@ -0,0 +1,85 @@
+﻿using FamilyTreeBuilder.Model;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace FamilyTreeBuilder
+{
+    /// <summary>
+    /// Interaction logic for Field.xaml
+    /// </summary>
+    public partial class Field : UserControl
+    {
+        private List<Human> humans;
+        private Point cameraPosition;//Переделать на "глобальную завязку" 
+        private Point oldPosition;
+
+        public Field()
+        {
+            InitializeComponent();
+            humans = new List<Human>();
+            var human = new Human();
+            Human.Width = 300;
+            Human.Height = 150;
+            human.Position = new Point(100,100);
+            humans.Add(human);
+            cameraPosition = new Point( 0, 0);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(43, 43, 43));
+            drawingContext.DrawRectangle(brush, null, new Rect() { Location = new Point(0, 0), Height = ActualHeight, Width = ActualWidth }) ;
+            var rect = new Rect();
+            rect.Size = new Size(Human.Width, Human.Height);
+            brush = new SolidColorBrush(Color.FromRgb(43, 43, 43));
+            Pen pen = new Pen(new SolidColorBrush(Color.FromRgb(160, 160, 160)), 1);
+            foreach (var item in humans)
+            {
+                rect.Location = new Point(item.Position.X - cameraPosition.X, item.Position.Y - cameraPosition.Y );
+                drawingContext.DrawRectangle(brush, pen, rect);
+            }
+        }
+
+        private Human GetHumanOnLocation(Point location)
+        {
+            foreach (var item in humans)
+            {
+                if(item.CheckClick(location))
+                    return item;
+            }
+            return null;
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            oldPosition = e.GetPosition(this);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+                cameraPosition = new Point(e.GetPosition(this).X - oldPosition.X, e.GetPosition(this).Y - oldPosition.Y);
+                InvalidateVisual();
+            }
+        }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            var human = new Human();
+            var pos = new Point(e.GetPosition(this).X - Human.Width / 2, e.GetPosition(this).Y - Human.Height / 2);
+            human.Position = pos;
+            humans.Add(human);
+            InvalidateVisual();
+        }
+    }
+}
